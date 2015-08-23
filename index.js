e600386f4ec2250a2814b8993d09fad87cbe302a
A.D. Slaton <ad.slaton@turner.com>
2015-08-23 00:12:53 -0400

    Adding dot files

--- index.js
+++ index.js
@@ -1,12 +1,55 @@
+'use_strict';
+
 var express = require('express'),
     app = express(),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    session = require('express-session'),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local'),
     config = require('./config.js'),
     server;
 
-app.get('/', function(req, res) {
+// ===============EXPRESS================
+// Configure Express
+app.use(logger('combined'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(session({secret: 'supernova', saveUninitialized: true, resave: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Session-persisted message middleware
+app.use(function (req, res, next) {
+    var err = req.session.error,
+        msg = req.session.notice,
+        success = req.session.success;
+
+    delete req.session.error;
+    delete req.session.success;
+    delete req.session.notice;
+
+    if (err) {
+        res.locals.error = err;
+    }
+    if (msg) {
+        res.locals.notice = msg;
+    }
+    if (success) {
+        res.locals.success = success;
+    }
+
+    next();
+});
+
+app.get('/', function (req, res) {
     res.send('Hello world');
 });
 
-server = app.listen(config.port, function() {
+server = app.listen(config.port, function () {
     console.log('Authenticate is listening on port:%s', config.port);
 });
