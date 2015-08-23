ef44908d7ea965d4fd9e8c7d16d5018b8200f8e0
A.D. Slaton <slaton.ad@gmail.com>
2015-08-23 18:45:27 +0000

    Adding generic routes

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,25 @@
 var express = require('express'),
     router = express.Router();
 
-router.get('/', function (req, res, next) {
+router.get('/_hc', function (req, res, next) {
     res.json({status: 200});
 });
+
+router.get('/login', function (req, res, next) {
+    res.json({status: 200, type: 'login'});
+});
+
+router.post('/signup', function (req, res, next) {
+    var body = req.body;
+    
+    res.json({status: 200, data: body}); 
+});
+
+router.post('/update/:id', function (req, res, next) {
+    var id = req.params.id,
+        body = req.body;
+    
+    res.json({status: 200, id: id, data: body});
+})
 
 module.exports = router;
