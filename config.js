b32f91eb398d8798a58b3f33e6c7cc01376722f7
A.D. Slaton <slaton.ad@gmail.com>
2015-08-29 17:30:31 +0000

    Moving values to the environment

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    email: process.env.EMAIL || false,
+    mongo: process.env.MONGO_URL,
+    port: process.env.PORT || 5800,
+    sendMail: {
+        from: '',
+        subject: ''
+    }
+};
