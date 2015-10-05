9ab2be30d80957e15d5e538337ee959508e332de
adslaton <slaton.ad@gmail.com>
2015-10-05 23:41:19 +0000

    Updating config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
     mongo: process.env.MONGO_URL,
     port: process.env.PORT || 5800,
     sendMail: {
-        from: '',
-        subject: ''
+        from: process.env.EMAIL_FROM,
+        subject: process.env.EMAIL_SUBJECT
     }
 };
